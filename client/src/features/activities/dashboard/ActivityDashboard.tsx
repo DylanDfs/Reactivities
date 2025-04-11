cc45f445804d9d20522d6146624ebff11bfe3150
DDE24699 <dylan.defives@open-groupe.com>
2025-04-11 11:35:13 +0200

    End of section 21

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,13 +3,19 @@ import ActivityList from "./ActivityList";
 import ActivityFilters from "./ActivityFilters";
 
 export default function ActivityDashboard() {
-
     return (
         <Grid2 container spacing={3}>
             <Grid2 size={7}>
                 <ActivityList />
             </Grid2>
-            <Grid2 size={5}>
+            <Grid2 
+                size={5}
+                sx={{
+                    position: 'sticky',
+                    top: 112,
+                    alignSelf: 'flex-start'
+                }}
+            >
                 <ActivityFilters />
             </Grid2>
         </Grid2>
